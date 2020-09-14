13bc8e18eba3f9ea95313dab2abf78a55185670a
Mac Morgan <mkatjounis@yahoo.fr>
2020-09-14 17:10:41 -0400

    Debut exercice cours 3 (Bootstrap)

--- usersView.php
+++ usersView.php
@@ -1,5 +1,5 @@
 <?php
-    echo '<table><tr><td><b>First Name<b/><td/><td><b>Last Name<b/><td/><tr/>';
+    echo '<table class=\"table\"><tr><th scope=\"col\"><b>First Name<b/><th/><th scope=\"col\"><b>Last Name<b/><th/><tr/>';
     foreach($users as $user)
     {
         echo "<tr><td>".($user->get_first_name())."<td/><td>".($user->get_last_name())."<td/><tr/>";
